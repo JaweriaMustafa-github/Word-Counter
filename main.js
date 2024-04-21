be1b6890c651f9d3e429dff2877f911f07aef776
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-04-21 23:28:32 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+// Print the word count of the sentence
+console.log(chalk.cyanBright `Your sentence word count is ${words.length}`);
